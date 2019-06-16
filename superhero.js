86bf67d4ddc1898589cb832cc42ce281ec6ea887
Carlos Dasaed Salcedo Carreno <cdsalcedo@mun.ca>
2019-06-16 11:01:21 -0230

    dasaed-maclinux 06/16/19-11:01:21: quick modification

---
+++ superhero.js
@@ -0,0 +1,30 @@
+        {
+	    "name" : "Spiderman",
+	    "realName":"Peter Parker",
+ 	    "superPowers" : ['Super Strength', 'Spidersense', 'Sticks to walls', 'Super agility'],
+	    "hero" : True
+        },
+        {
+	    "name" : "Thor",
+	    "realName":"Thor Odinson",
+ 	    "superPowers" : ['Super Strength', 'Silver tongue', 'Controls Lighting', 'Flight', 'Controls the Weather'],
+	    "hero" : True
+        },
+        {
+	    "name" : "Wolverine",
+	    "realName":"Logan",
+ 	    "superPowers" : ['Super Strength', 'Accelerated Healing Factor', 'Ademantium Skeleton', 'Ademantium Claws'],
+	    "hero" : True
+        },
+        {
+	    "name" : "Captain America",
+	    "realName":"Steve Rogers",
+ 	    "superPowers" : ['Super Strength', 'Accelerated Healing Factor', 'Enhanced Human abilities'],
+	    "hero" : True
+        },
+        {
+	    "name" : "Ironman",
+	    "realName":"Tony Stark",
+ 	    "superPowers" : ['Excessively Wealthy', 'Super Smart', 'Posses an iron suit'],
+	    "hero" : True
+        }
